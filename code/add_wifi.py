cc09ae4fa99d3009607a69fa0419e55f70784aef
nackjaylor <jacknaylor1998@gmail.com>
2023-03-17 17:29:32 +1100

    Fix firewall lockout

--- code/add_wifi.py
+++ code/add_wifi.py
@@ -14,21 +14,36 @@ def main():
     password = input('Enter Password: ')
     static_ip = input('Static IP Address: ')
     print('---------------')
-
-    network_config = \
-        "    wlan0:\n" + \
-        "      dhcp4: yes\n" + \
-        "      dhcp6: yes\n" + \
-        "      addresses: [{}/32]\n".format(static_ip) + \
-        "      optional: true\n" + \
-        "      access-points:\n" + \
-        "        {}:\n".format(ssid) + \
-        "          password: {}\n".format(password)
+    if ssid == "KIRBYBOTS":
+        network_config = \
+            "    wlan0:\n" + \
+            "      dhcp4: no\n" + \
+            "      dhcp6: no\n" + \
+            "      addresses: [{}/32]\n".format(static_ip) + \
+            "      optional: true\n" + \
+            "      access-points:\n" + \
+            "        {}:\n".format(ssid) + \
+            "          password: {}\n".format(password) + \
+            "      nameservers:\n" + \
+            "	 addresses: [172.17.33.1, 172.17.7.242, 172.17.7.243]" + \
+            "      routes:" + \
+            "        - to: default" + \
+            "        via: 172.17.33.254"
+    else:
+        network_config = \
+            "    wlan0:\n" + \
+            "      dhcp4: yes\n" + \
+            "      dhcp6: yes\n" + \
+            "      addresses: [{}/32]\n".format(static_ip) + \
+            "      optional: true\n" + \
+            "      access-points:\n" + \
+            "        {}:\n".format(ssid) + \
+            "          password: {}\n".format(password)
 
     with open(network_file, "a") as file_object:
         file_object.write(network_config)
 
     print("Network configured:\n" + network_config + "---------------")
 
-if '__main__':
+if __name__ == '__main__':
     main()
